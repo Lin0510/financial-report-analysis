0ca89f525893d3b6dbfd8696aede3042d7087192
林俞均 <anita.lin@promeritage.com.tw>
2022-09-19 11:59:18 +0800

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/report">財報分析</router-link> |
+    <router-link to="/moat">護城河優勢</router-link> |
+    <router-link to="/risk">風險扣分</router-link>
   </nav>
   <router-view/>
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,21 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/FinancialReportAnalysis.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'home',
+    path: '/report',
+    name: 'report',
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/moat',
+    name: 'moat',
+    component: () => import('../views/Moat.vue')
+  },
+  {
+    path: '/risk',
+    name: 'risk',
+    component: () => import('../views/Risk.vue')
   }
 ]
 
