b250a59a17e1dc048ebad6232a9c4ec56f93151b
林俞均 <anita.lin@promeritage.com.tw>
2023-06-01 16:59:15 +0800

    新增按下enter觸發確認方法

--- src/components/FinancialReport.vue
+++ src/components/FinancialReport.vue
@@ -4,7 +4,7 @@
       <div class="col-md-4 col-sm-10">
         <div class="input-group has-validation">
           <input v-model="stock" @input="restrictInput($event)" maxlength="5" class="form-control" placeholder="請輸入股票代碼，只能輸入英文字母和."
-            :class="{ 'is-invalid': isStockEmpty || !stockExsits}" :disabled="isDisabled" />
+            :class="{ 'is-invalid': isStockEmpty || !stockExsits}" :disabled="isDisabled" @keyup.enter="confirm()"/>
           <div v-if="isStockEmpty || !stockExsits" class="invalid-feedback">
             {{ errorMessage }}
           </div>
